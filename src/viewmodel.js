f127908d7d39e42e99c6dbb8cfd491126cea9d68
Boris Vanyushin <b.vanyushin@gmail.com>
2016-05-02 22:26:05 +0200

    add styles for different cells types

---
+++ src/viewmodel.js
@@ -0,0 +1,18 @@
+/* global window */
+module.exports = (function() {
+  'use strict';
+
+  var battleField = window.document.getElementById('battle-field');
+  var classes = ['empty', 'miss', 'hit', 'destroyed'];
+  for (var i = 0; i < 10; i++) {
+    var row = window.document.createElement('div');
+    row.classList.add('row');
+    for (var j = 0; j < 10; j++) {
+      var cell = window.document.createElement('div');
+      cell.classList.add('cell');
+      cell.classList.add(classes[i % 4]);
+      row.appendChild(cell);
+    }
+    battleField.appendChild(row);
+  }
+})();
